1ba7fec77fa15e24671b2b398f6a5fa6837c73cb
Michael Duffy <MDuffy@asginc.us>
2020-08-05 19:25:43 +0100

    add About.js

---
+++ src/component/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>This App uses React, Redux, React Router, and many other helpful libraries</p>
+        </div>
+    )
+}
+
+export default About;
