0c02c87cae1e6d32cd0b6ed2f9fdcdf83bd7e91a
Michael Duffy <MDuffy@asginc.us>
2020-08-09 22:18:29 +0100

    use thunk for course load

--- src/component/courses/CoursesPages.js
+++ src/component/courses/CoursesPages.js
@@ -3,41 +3,21 @@ import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPages extends Component {
-	state = {
-		course: {
-			title: '',
-		},
-	};
-
-	handleChange = event => {
-		const course = { ...this.state.course, title: event.target.value };
-		this.setState({ course: course });
-	};
-
-	handleSubmit = event => {
-		event.preventDefault();
-		this.props.actions.createCourse(this.state.course);
-	};
+	componentDidMount() {
+		this.props.actions
+			.loadCourses()
+			.catch(error => alert('Course load failed ' + error));
+	}
 
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<>
 				<h2>Courses</h2>
-				<h3>Add Course</h3>
-				<input
-					type='text'
-					value={this.state.course.title}
-					onChange={this.handleChange}
-				/>
-				<input type='submit' value='save' />
-				<ul>
-					{this.props.courses.map(course => (
-						<li key={course.title}>{course.title}</li>
-					))}
-				</ul>
-			</form>
+				<CourseList courses={this.props.courses} />
+			</>
 		);
 	}
 }
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,24 @@
 import * as types from './actionTypes';
+import * as courseApi from '../../api/courseApi';
+
+export const loadCoursesSuccess = courses => {
+	return { type: types.LOAD_COURSES_SUCCESS, courses: courses };
+};
 
 export const createCourse = course => {
 	return { type: types.CREATE_COURSE, course: course };
+};
+
+export const loadCourses = () => {
+	return dispatch => {
+		return courseApi
+			.getCourses()
+			.then(courses => {
+				dispatch(loadCoursesSuccess(courses));
+			})
+			.catch(error => {
+				console.log(error);
+				throw error;
+			});
+	};
 };
