c58786ab49e6b4037712c74beb875b0d46284d1b
michael-irl <iszekeell@gmail.com>
2020-09-25 21:46:45 +0100

    update manage course page

--- src/component/courses/ManageCoursesPage.js
+++ src/component/courses/ManageCoursesPage.js
@@ -4,33 +4,38 @@ import * as courseActions from '../../redux/actions/courseActions';
 import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 
-const ManageCoursesPage = ({ loadAuthors, loadCourses }) => {
-	useEffect(() => {
-		loadAuthors();
-		loadCourses();
-	}, []);
+const ManageCoursesPage = ({ authors, courses, loadAuthors, loadCourses }) => {
+  useEffect(() => {
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => alert('Author load failed ' + error));
+    }
 
-	return <div>Manage Courses</div>;
+    if (courses.length === 0) {
+      loadCourses().catch((error) => alert('Course load failed ' + error));
+    }
+  }, []);
+
+  return <div>Manage Courses</div>;
 };
 
 ManageCoursesPage.propTypes = {
-	courses: PropTypes.array.isRequired,
-	authors: PropTypes.array.isRequired,
-	actions: PropTypes.object.isRequired,
-	loadAuthors: PropTypes.func.isRequired,
-	loadCourses: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  loadCourses: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = {
-	loadCourses: courseActions.loadCourses,
-	loadAuthors: authorActions.loadAuthor,
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthor,
 };
 
 const mapStateToProps = function (state) {
-	return {
-		courses: state.courses,
-		authors: state.authors,
-	};
+  return {
+    courses: state.courses,
+    authors: state.authors,
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursesPage);
