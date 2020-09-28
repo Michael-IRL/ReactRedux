1729215532500bd36c9f367e1ad07d787d7e8ae1
Michael Duffy <MDuffy@asginc.us>
2020-09-28 11:43:04 +0100

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './component/App';
